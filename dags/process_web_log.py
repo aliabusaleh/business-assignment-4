4c370aea1342f13c92df924f3503d0468946366d
Lorenc1o <joseantoniolorencio@gmail.com>
2022-12-24 18:15:57 +0100

    Just formatted some things

--- dags/process_web_log.py
+++ dags/process_web_log.py
@@ -6,11 +6,7 @@ from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.sensors.file_sensor import FileSensor
 
-
 log = logging.getLogger(__name__)
-
-
-
 
 default_args = {
     'dag_id': 'process_web_log',
@@ -35,19 +31,14 @@ extract_logs = BashOperator(
     bash_command="grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /opt/airflow/dags/the_log/log.txt > /opt/airflow/dags/the_log/extracted_data.txt",
     dag=dag)
 
-
-
 filter_logs = BashOperator(
     task_id='transform_data',
     bash_command="awk '!/198.46.149.143/' /opt/airflow/dags/the_log/extracted_data.txt > /opt/airflow/dags/the_log/log_filterd.txt",
     dag=dag)
 
-
 zip_data = BashOperator(
     task_id='load_data',
     bash_command="tar -czvf /opt/airflow/dags/the_log/weblog.tar /opt/airflow/dags/the_log/log_filterd.txt",
     dag=dag)
-
-
 
 scanForLog >> extract_logs >> filter_logs >> zip_data
